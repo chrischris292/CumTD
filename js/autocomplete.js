34449bf5bb3de7337b78059c4157ac6a6e534bfb
chrischris292 <cbchan2@illinois.edu>
2013-11-20 20:59:15 -0600

    it is slow and messy as hell

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -9,20 +9,23 @@ var data = "";
 var markers = [];
 var iterator = 0;
 $(document).ready(function()
-{
-$("#help").click(function(){introJs().start();})
- var champaign = new google.maps.LatLng(40.106831, -88.227425);
- google.maps.event.addDomListener(window, 'load', initialize);
- drop();
- $('#busStop').typeahead([
-  {
-    name: 'results',
-    valueKey: 'n',
-    remote: 'http://www.cumtd.com/autocomplete/stops/v1.0/json/search?query=%QUERY',
-    template: '<p><strong>{{n}}</strong></p>',
-    engine: Hogan
-  }
-]);
+	{
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+//Typeahead Support
+	$('#busStop').typeahead([
+	{
+		name: 'results',
+		valueKey: 'n',
+		remote: 'http://www.cumtd.com/autocomplete/stops/v1.0/json/search?query=%QUERY',
+		template: '<p><strong>{{n}}</strong></p>',
+		engine: Hogan
+	}
+	]);
+	$("#help").click(function()
+	{
+			introJs().start();
+	})
 
 });			
 
@@ -35,26 +38,44 @@ var berlin = new google.maps.LatLng(52.520816, 13.410186); Starter
 var champaign = new google.maps.LatLng(40.106831, -88.227425);
 var busStopMarkers = [
   new google.maps.LatLng(40.106831, -88.227425),
-  new google.maps.LatLng(40.104831, -88.227425),
-  new google.maps.LatLng(40.106331, -88.227425),
-  new google.maps.LatLng(40.106831, -88.227425)
 ];
 
 var markers = [];
 var iterator = 0;
-
 var map;
 
 function initialize() {
+
   var mapOptions = {
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     center: champaign
   };
-
   map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+  //drop();
+  var theData = getAllStops(map);
+  stops = theData.responseJSON.stops;
+		       	for(i=0;i<stops.length;i++)
+		       	{
+		       	theStop = stops[i];
+		       	console.log(theStop)
+				var marker = new google.maps.Marker(
+				{
+					position: new google.maps.LatLng (theStop.stop_points[0].stop_lat, theStop.stop_points[0].stop_lon),
+					map: map,
+					title: 'penis everywhere',
+				});
+		        }
+  var marker = new google.maps.Marker(
+	{
+		position: new google.maps.LatLng (40.106831, -88.227625),
+		map: map,
+		title: 'penis everywhere',
+	});
+
 }
 
+//Functions
 function drop() {
   for (var i = 0; i < busStopMarkers.length; i++) {
     setTimeout(function() {
@@ -63,6 +84,7 @@ function drop() {
 		map: map,
 		draggable: false,
 		title: "penis",
+		content: "big dicks",
 		animation: google.maps.Animation.DROP
 	}));
 	iterator++;
@@ -75,41 +97,19 @@ function drop() {
 
 var temp = [];
 
-	function getAllStops(){
-	c = "http://developer.cumtd.com/api/v2.2/json/GetStops?key=a6188b7a357a485b866197cab02c09f0"
-		$.ajax({
+function getAllStops(map){
+c = "http://developer.cumtd.com/api/v2.2/json/GetStops?key=a6188b7a357a485b866197cab02c09f0"
+	result = $.ajax({
 	        url: c,
 	        dataType: "json",
 			data: data,
-			async: true,
+			async: false,
 	        success: function(data) {
-			return data;
-	        }
+		       	return data;
+	    	}
 		  });
-	}
-
-
-function createMarker (vehicle, map) {
-  var marker = new RichMarker({
-      position: new google.maps.LatLng(vehicle.lat, vehicle.lon),
-      map: map,
-      title: vehicle.trip.shape_id,
-  });
-
-  var contentString = '<h1>' + vehicle.trip.route_id + " - " + vehicle.trip.direction + '</h1>';
-
-  google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map,marker);
-      infowindow.setContent(contentString)
-  });
-
-
-  infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-  
-  return marker;
+	return result;
 }
-
-
 	
+
+
